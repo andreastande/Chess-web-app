c09eb01bffd45d0a91690ecded77dbce1831a0f3
Andreas Tande <andretan@stud.ntnu.no>
2025-01-29 00:18:43 +0100

    Added rounded corners to chessboard

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,4 +1,4 @@
-import { isTileBlack } from "../utils/gameplayUtils"
+import { determineCorner, isTileBlack } from "../utils/gameplayUtils"
 
 const Play = () => {
   const tiles = [
@@ -17,26 +17,35 @@ const Play = () => {
       <div className="flex flex-col w-176 h-176">
         {tiles.map((row: number[], rowNumber) => (
           <div className="flex w-full h-22">
-            {row.map((_col: number, colNumber) => (
-              <div
-                className={`w-22 relative h-full ${isTileBlack(rowNumber, colNumber) ? "bg-[#AF8866]" : "bg-[#E6D5B2]"}`}
-              >
-                {colNumber === 0 && (
-                  <div
-                    className={`absolute top-1 left-2 ${isTileBlack(rowNumber, colNumber) ? "text-[#E6D5B2]" : "text-[#AF8866]"}`}
-                  >
-                    {8 - rowNumber}
-                  </div>
-                )}
-                {rowNumber === 7 && (
-                  <div
-                    className={`absolute bottom-1 right-2 ${isTileBlack(rowNumber, colNumber) ? "text-[#E6D5B2]" : "text-[#AF8866]"}`}
-                  >
-                    {["a", "b", "c", "d", "e", "f", "g", "h"][colNumber]}
-                  </div>
-                )}
-              </div>
-            ))}
+            {row.map((_col: number, colNumber) => {
+              let cornerClass = ""
+              const corner = determineCorner(rowNumber, colNumber)
+              if (corner === "tl") cornerClass = "rounded-tl-sm"
+              if (corner === "tr") cornerClass = "rounded-tr-sm"
+              if (corner === "bl") cornerClass = "rounded-bl-sm"
+              if (corner === "br") cornerClass = "rounded-br-sm"
+
+              return (
+                <div
+                  className={`w-22 relative h-full ${cornerClass} ${isTileBlack(rowNumber, colNumber) ? "bg-[#AF8866]" : "bg-[#E6D5B2]"}`}
+                >
+                  {colNumber === 0 && (
+                    <div
+                      className={`absolute top-1 left-2 select-none ${isTileBlack(rowNumber, colNumber) ? "text-[#E6D5B2]" : "text-[#AF8866]"}`}
+                    >
+                      {8 - rowNumber}
+                    </div>
+                  )}
+                  {rowNumber === 7 && (
+                    <div
+                      className={`absolute bottom-1 right-2 select-none ${isTileBlack(rowNumber, colNumber) ? "text-[#E6D5B2]" : "text-[#AF8866]"}`}
+                    >
+                      {["a", "b", "c", "d", "e", "f", "g", "h"][colNumber]}
+                    </div>
+                  )}
+                </div>
+              )
+            })}
           </div>
         ))}
       </div>
--- src/utils/gameplayUtils.ts
+++ src/utils/gameplayUtils.ts
@@ -1,3 +1,12 @@
 export const isTileBlack = (row: number, col: number) => {
   return (row + col) % 2 === 1
 }
+
+export const determineCorner = (row: number, col: number) => {
+  if (row === 0 && col === 0) return "tl" // Top-left
+  if (row === 0 && col === 7) return "tr" // Top-right
+  if (row === 7 && col === 0) return "bl" // Bottom-left
+  if (row === 7 && col === 7) return "br" // Bottom-right
+
+  return "" // Not a corner
+}
